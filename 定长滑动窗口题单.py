bacf20dfbc1db570a40879f6980c6aa79e9c7c02
zihang <zihangoole@gmail>
2025-01-13 05:19:58 +0000

    2025-01-13

--- 定长滑动窗口题单.py
+++ 定长滑动窗口题单.py
@@ -71,4 +71,94 @@ class Solution:
         
         return res
 
+# 1052.爱生气的书店老板  mid
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        n = len(customers)
+        
+        # 滑动窗口求得minutes内的最大收益, 让最多的人满意
+        # pos表示当前最大收益窗口的右边界
+        temp = 0
+        res = float('-inf')
+        pos = 0
+        for i, num in enumerate(customers):
+            if grumpy[i] == 1:
+                temp += num
+            
+            if i < minutes - 1:
+                continue
+            
+            if temp > res:
+                res = temp
+                pos = i
+                
+            # res = max(temp, res)
+
+            if grumpy[i - minutes + 1] == 1:
+                temp -= customers[i - minutes + 1]
+        
+        # 求出最后的结果。[pos-minutes+1, pos]区间内的都算满意。
+        res = 0
+        for i in range(n):
+            if grumpy[i] == 0:
+                res += customers[i]
+            elif i >= pos - minutes + 1 and i <= pos:
+                res += customers[i]
+
+        return res
+
+# 1461.检查一个字符串是否包含所有长度为 K 的二进制子串
+class Solution:
+    # 滑动串口+哈希记录所有的子串  最后判断是否有2^k个
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        n = len(s)
+        # 长度不够直接退出
+        if n < 2 ** k  + k - 1:
+            return False
+        
+        dic = {}
+        # i = 0
+        j = k - 1
+        while(j < n):
+            temp = s[j-k+1: j+1] 
+            if temp not in dic:
+                dic[temp] = 1
+            
+            j += 1
+        
+        if len(dic) == 2 ** k:
+            return True
+        
+        return False
+    
+# 2814.几乎唯一子数组最大和
+class Solution:
+    # 滑动窗口变形。普通的定长滑动窗口加上一个条件判断，然后计算res
+    def maxSum(self, nums: List[int], m: int, k: int) -> int:
+        n = len(nums)
+
+        res = 0
+        temp = 0
+        dic = {}
+        for i, num in enumerate(nums):
+            temp += num
+            if num not in dic:
+                dic[num] = 1
+            else:
+                dic[num] += 1
+                
+            if i < k - 1:
+                continue
+            
+            # s = set(nums[i-k+1: i+1])
+            if len(dic) >= m:
+                res = max(res, temp)
+            
+            temp -= nums[i-k+1]
+            if dic[nums[i-k+1]] == 1:
+                del dic[nums[i-k+1]]
+            else:
+                dic[nums[i-k+1]] -= 1
+        
+        return res
 
