a36e670899255042b87aefc76009c6520b9ef15c
zihang <zihangoole@gmail>
2025-01-16 01:57:57 +0000

    2025-01-16

--- 定长滑动窗口题单.py
+++ 定长滑动窗口题单.py
@@ -1,3 +1,21 @@
+from typing import List
+'''
+总结
+1、模板题单指针写法和双指针写法 
+    单指针 lc1456
+    双指针 1652
+2、针对滑动窗口内满足某个的条件的res值
+    2.1 窗口求平均满足某个条件  lc1343
+    2.2 窗口内不重复元素条件  lc1297 lc2461 lc2814
+    2.3 窗口内第x小数  lc2653
+3、问题转变为滑动窗口题
+    lc1423  k = n - k
+    lc2090 半径为k的子数组平均值 k = 2*k+1
+    lc2134 最小交换次数把所有1放一起  k = sum(nums)
+4、k属于区间[minSize, maxSize]，遍历k的范围，然后计算res
+    lc1297
+'''
+
 # 1456.定长子串元音最大数目
 class Solution:
     # 定长滑动窗口模板套路
@@ -71,7 +89,7 @@ class Solution:
         
         return res
 
-# 1052.爱生气的书店老板  mid
+# 1052.爱生气的书店老板 mid
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         n = len(customers)
@@ -161,4 +179,186 @@ class Solution:
                 dic[nums[i-k+1]] -= 1
         
         return res
+    
+# 2461.长度为 K 子数组中的最大和  mid
+class Solution:
+    # 常规变形：定长滑动+子数组特定条件
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        
+        res = 0
+        temp = 0
+        dic = {}
+        
+        for i, num in enumerate(nums):
+            temp += num
+            if num not in dic:
+                dic[num] = 1
+            else:
+                dic[num] += 1
 
+            if i < k - 1:
+                continue
+
+            if len(dic) == k:
+                res = max(res, temp)
+
+            leftNum = nums[i-k+1]
+            temp -= leftNum
+            if dic[leftNum] == 1:
+                del dic[leftNum]
+            else:
+                dic[leftNum] -= 1
+            
+        return res
+            
+# 1423.一行卡牌可获得的最大点数  mid
+class Solution:
+    # 转换为滑动窗口n-k的最小和
+    # 注意：1、k==n的情况，此时窗口等于0
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        n = len(cardPoints)
+        s = sum(cardPoints)
+
+        if k == n:
+            return s
+        new_k = n - k
+        
+        temp = 0
+        res = float('-inf')
+        for i, num in enumerate(cardPoints):
+            temp +=  num
+            if i < new_k - 1:
+                continue
+            
+            res = max(res, s - temp)
+            
+            # print(i)
+            temp -= cardPoints[i - new_k + 1]
+        
+        return res
+
+# 1652.拆炸弹
+class Solution:
+    # 学习：1、循环数组采用 code += code拼接。 2、学习使用滑动窗口的双指针写法。之前属于单指针写法[i-k+1, i]表示窗口区间
+    # 题解：拼接后滑动窗口  
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        n = len(code)
+        if k == 0:
+            return [0] * n
+        
+        code += code
+        res = []
+        
+        if k > 0:
+            l = 1
+            r = k
+        else:
+            r = n - 1
+            # k < 0 
+            l = n + k
+        
+        # for i in range()
+        s = sum(code[l:r+1])
+        for i in range(n):
+            res.append(s)
+            s -= code[l]
+            s += code[r + 1]
+            
+            l += 1
+            r += 1
+        
+        return res
+
+# 1297.子串的最大出现次数  mid
+class Solution:
+    # 简单变形题：k范围[minSize, maxSize],区间内不同字符<=maxLetters
+    def maxFreq(self, s: str, maxLetters: int, minSize: int, maxSize: int) -> int:
+        n = len(s)
+        
+        res = 0
+        for k in range(minSize, maxSize+1):
+            dic = {}
+            ch_dic = {}
+        
+            for i, ch in enumerate(s):
+                # temp += ch
+                if ch not in ch_dic:
+                    ch_dic[ch] = 1
+                else:
+                    ch_dic[ch] += 1
+
+                if i < k - 1:
+                    continue
+                
+                temp = s[i-k+1:i+1]
+                if len(ch_dic) <= maxLetters:
+                    if temp not in dic:
+                        dic[temp] = 1
+                    else:
+                        dic[temp] += 1
+                
+                    res = max(res, dic[temp])
+
+                l_ch = s[i-k+1]
+                if ch_dic[l_ch] == 1:
+                    del ch_dic[l_ch]
+                else:
+                    ch_dic[l_ch] -= 1
+                
+        return res
+
+# 2134.最小交换次数把所有1放一起  mid
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        n = len(nums)
+        k = sum(nums)
+        nums += nums
+
+        if k == 0:
+            return 0
+        
+        temp = 0
+        res = float('inf')
+        for i, num in enumerate(nums[:n+k-1]):
+            temp += num
+            if i < k - 1:
+                continue
+            
+            res = min(res, k - temp)
+            
+            temp -= nums[i-k+1]
+        
+        return res
+
+# 2653.滑动子数组内的第x小数  
+class Solution:
+    # 如何求窗口内第x小数：由于值域很小，采取计数排序的方法。累计计数，第一个大于x的数就是第x小数。
+    def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
+        n = len(nums)
+        
+        res = []
+        window = [0] * 101
+        for i, num in enumerate(nums):
+            window[num+50] += 1
+            if i < k - 1:
+                continue
+            
+            count = 0
+            # print(window)
+            for j in range(len(window)):
+                count += window[j]
+                if count >= x:
+                    if j - 50 < 0:
+                        res.append(j - 50)
+                    else:
+                        res.append(0)
+
+                    break
+            
+            window[nums[i-k+1]+50] -= 1
+        
+        return res
+                    
+                
+            
